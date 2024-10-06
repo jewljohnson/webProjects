d3e5cdcde320267d90c21cefde2ae161ee0b0007
Jewl Johnson <jewljohnson43@gmail.com>
2024-10-06 13:10:08 -0400

    first commit since starting devslopes

---
+++ learnReact/HelloWorld/src/Components/ListGroup.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function ListGroup() {
+    let items = [
+        'Atlanta',
+        'Chicago',
+        'St. Louis',
+        'Houston',
+        'Cincinatti'
+    ];
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    return <>
+        <h1>Cities</h1>
+        {items.length === 0 && <p>No item found</p>}
+        <ul className="list-group">
+            {items.map((item, index) =>
+                <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}
+                    key={item}
+                    onClick={() => { setSelectedIndex(index); }}
+                >
+                    {item}
+                </li>)}
+        </ul>
+    </>
+}
+
+export default ListGroup;
